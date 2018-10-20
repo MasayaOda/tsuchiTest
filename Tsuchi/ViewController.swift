4ae687cdb98dc1a698b16df68f092f252c2e0891
小田将也 <odadmsy0630@gmail.com>
2018-10-20 09:59:17 +0900

    Initial Commit

---
+++ Tsuchi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tsuchi
+//
+//  Created by 小田将也 on 2018/10/20.
+//  Copyright © 2018年 odamasaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
