3edbd74122b54b292861114d58edb1d52968cc5d
小田将也 <odadmsy0630@gmail.com>
2018-10-20 21:32:02 +0900

    最初

--- Tsuchi/ViewController.swift
+++ Tsuchi/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import UserNotifications // これのインポートが必須
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 通知を使用可能にする設定
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) {
+            (granted, error) in
+            // エラー処理
+        }
+        
+        // 通知自体の設定
+        let content = UNMutableNotificationContent()
+        
+        content.title = NSString.localizedUserNotificationString(forKey: "通知タイトル", arguments: nil)
+        content.body = NSString.localizedUserNotificationString(forKey: "通知するメッセージ", arguments: nil)
+        content.sound = UNNotificationSound.default
+        
+        // アプリを起動して5秒後に通知を送る
+        
+        var fireDate = DateComponents()
+        fireDate.hour = 10
+        fireDate.minute = 9
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: fireDate, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "my-notification", content: content, trigger: trigger)
+        
+        // 通知を登録
+        center.add(request) { (error : Error?) in
+            if error != nil {
+                // エラー処理
+            }
+        }
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
-
